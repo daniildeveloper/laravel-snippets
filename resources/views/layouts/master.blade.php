06dc5127cdbf56f826c6538d2f716a67c043b8f4
daniiltserin <daniilborowkow@yandex.ru>
2016-12-09 03:32:07 +0600

    Initial commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield("title")</title>
+    <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    @yield("styles")
+</head>
+<body>
+@include("partials.header")
+@yield("content")
+
+
+<script src="/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
