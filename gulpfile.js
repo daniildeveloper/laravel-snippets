0a2f739097d855cd22643e8fc06d1196007f92c8
daniiltserin <daniilborowkow@yandex.ru>
2017-05-05 19:58:34 +0600

    Created base logic for laravel-elixir-pug

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const elixir = require('laravel-elixir');
 
 require('laravel-elixir-vue-2');
 
+require('laravel-elixir-pug');
+
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -16,4 +18,38 @@ require('laravel-elixir-vue-2');
 elixir(mix => {
     mix.sass('app.scss')
        .webpack('app.js');
+});
+
+// compile less to css
+// elixir(function(mix) {
+//     mix.less('app.less');
+// });
+
+elixir(function(mix) {
+    mix.less([
+        'app.less',
+        'controllers.less'
+    ]);
+});
+
+elixir(function (mix) {
+    mix
+        .pug({
+            // Compile to blade.php files or html files
+            blade: false,
+            // Pretty output or uglified
+            pretty: true,
+            // Source of pug files
+            src: 'resources/assets/pug/',
+            // Files to look for, useful if you are still naming files .jade
+            search: '**/*.pug',
+            // Files to skip, useful for partials
+            exclude: '_partials/**/*',
+            // Extension of pug files. Only needed to be set if still naming file .jade
+            pugExtension: '.pug',
+            // If blade is true, output to resources/views, otherwise public/html
+            dest: 'public/html',
+            // Any additional watches
+            additional_watches: []
+        });
 });
