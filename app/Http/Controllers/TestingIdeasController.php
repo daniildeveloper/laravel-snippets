d74d7bb06d706d2667c9e692165db21d71209402
daniiltserin <daniilborowkow@yandex.ru>
2017-06-21 13:54:11 +0600

    Base email preview

--- app/Http/Controllers/TestingIdeasController.php
+++ app/Http/Controllers/TestingIdeasController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\TestMail;
 use Illuminate\Http\Request;
+use Mail;
 
 class TestingIdeasController extends Controller
 {
     public function test(Request $request)
+    {
+        Mail::to("daniilborowkow@yandex.ru")->send(new TestMail());
+    }
+
+    public function sendPay()
     {
         $pay = \Epay::basicAuth([
             'order_id' => 01111111111,
